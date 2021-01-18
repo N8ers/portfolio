039482829d0950511fd5439f52db4baa227394b9
nathan sheryak <nathanraysheryak@gmail.com>
2021-01-18 11:20:46 -0500

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+    <Home />
   </div>
 </template>
+
+<script>
+import Home from './components/Home.vue';
+
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome to my portfolio</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style>
+</style>

00a7fd7bca8e1616081399db3e1e3ae91ab93821
Nathan <nathanraysheryak@gmail.com>
2021-04-17 14:36:28 -0400

    get nav stuff working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,84 +1,54 @@
 <template>
+  <div>
+    <v-app-bar app>       
+      <v-toolbar-title>
+        <span class="font-weight-medium">Nathan Sheryak</span> 
+        <span class="grey--text"> | software developer</span>
+      </v-toolbar-title>
+    
+      <v-spacer></v-spacer>
 
-<div>
-  <v-app-bar app>
-        
-    <v-toolbar-title>Nathan Sheryak</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-app-bar-nav-icon 
-      @click="drawer = true" 
-      class="d-flex d-sm-none"
-    ></v-app-bar-nav-icon>
-  
-      <!-- NAV BAR -->
-      <template v-slot:extension>
-        <v-tabs v-model="tab" align-with-title class="d-none d-sm-flex">
-          <v-tabs-slider color="yellow"></v-tabs-slider>
-          <v-tab v-for="item in items" :key="item">
-            {{ item }}
-          </v-tab>
-        </v-tabs>
-      </template>
-      <!-- NAV BAR -->
+      <v-app-bar-nav-icon 
+        @click="drawer = true" 
+        class="d-flex d-md-none"
+      ></v-app-bar-nav-icon>
+
+      <v-navigation-drawer v-model="drawer" app absolute right>
+        <v-list>
+          <v-list-item @click="navigationDrawerSelected('about')">
+              <v-list-item-content>
+                  <v-list-item-title class="primary--text">About</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="navigationDrawerSelected('projects')">
+              <v-list-item-content>
+                  <v-list-item-title class="primary--text">Projects</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="navigationDrawerSelected('blogs')"> 
+              <v-list-item-content>
+                  <v-list-item-title class="primary--text">Blogs</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="navigationDrawerSelected('contact')">
+              <v-list-item-content>
+                  <v-list-item-title class="primary--text">Contact</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
+        </v-list> 
+      </v-navigation-drawer>
+
+      <div v-if="!this.drawer">
+        <v-tabs align-with-title class="d-none d-md-flex">
+          <v-tab @click="$vuetify.goTo('#about')">About</v-tab>
+          <v-tab @click="$vuetify.goTo('#projects')">Projects</v-tab>
+          <v-tab @click="$vuetify.goTo('#blogs')">Blogs</v-tab>
+          <v-tab @click="$vuetify.goTo('#contact')">Contact</v-tab>
+        </v-tabs> 
+      </div>
 
     </v-app-bar>
-
-
-
-    <!-- EXPANDED HAMBURGER -->
-    <v-navigation-drawer v-model="drawer" absolute temporary right>
-      <v-list nav dense>
-        <v-list-item-group>
-          <v-list-item v-for="(item, index) in sections" :key="item">
-            <v-list-item-title @click="tab = index">hello2! {{ item }}</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <!-- EXPANDED HAMBURGER -->
-
-</div>
-  <!-- <v-app-bar color="" dense dark fixed app> -->
-
-    <!-- <v-app-bar-title>Nathan Sheryak</v-app-bar-title> -->
-
-    <!-- <v-spacer></v-spacer> -->
-
-    <!-- <v-app-bar-nav-icon 
-      @click="drawer = true" 
-      class="d-flex d-sm-none"
-    ></v-app-bar-nav-icon> -->
-
-    <!-- <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      bottom
-      temporary
-      right
-    >
-      <v-list nav dense>
-
-        <v-list-item-group>
-          <v-list-item>
-            <v-list-item-title @click="navigationDrawerSelected('about')">About</v-list-item-title>
-            <v-list-item-title @click="navigationDrawerSelected('projects')">Projects</v-list-item-title>
-            <v-list-item-title @click="navigationDrawerSelected('blogs')">Blogs</v-list-item-title>
-            <v-list-item-title @click="navigationDrawerSelected('contact')">Contact</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer> -->
-
-    <!-- <div v-if="!this.drawer">
-      <v-tabs align-with-title class="d-none d-sm-flex">
-        <v-tab @click="$vuetify.goTo('#about')">About</v-tab>
-        <v-tab @click="$vuetify.goTo('#projects')">Projects</v-tab>
-        <v-tab @click="$vuetify.goTo('#blogs')">Blogs</v-tab>
-        <v-tab @click="$vuetify.goTo('#contact')">Contact</v-tab>
-      </v-tabs> 
-    </div> -->
-
-  <!-- </v-app-bar> -->
+  </div>
 </template>
 
 <script>
@@ -90,9 +60,6 @@ export default {
     items: [
       'About', 'Projects', 'Blogs', 'Contact'
     ],
-    sections: [
-      'About', 'Projects', 'Blogs', 'Contact'
-    ]
   }),
   methods: {
     navigationDrawerSelected: function (location) {

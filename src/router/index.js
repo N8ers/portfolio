6655f28806a9b21fc6c4c815ffb52b01fade9584
Nathan <nathanraysheryak@gmail.com>
2021-04-23 07:52:25 -0400

    remove unused home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../components/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
+
 ];
 
 const router = new VueRouter({

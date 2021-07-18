44db95d9f8f1df742d1f134fbc3dd54a20b3e7dd
Nathan <nathanraysheryak@gmail.com>
2021-07-18 09:32:09 -0400

    eslint + add postgres docs to blogs

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 export default {
   projects: [
     {
-      name: 'Dashboard',
-      description: 'A landing page to organize your day.',
-      github: 'https://github.com/N8ers/dashboard',
-      site: 'https://dashboard-299215.web.app/welcome',
-      image: 'dashboard.png',
+      name: "Dashboard",
+      description: "A landing page to organize your day.",
+      github: "https://github.com/N8ers/dashboard",
+      site: "https://dashboard-299215.web.app/welcome",
+      image: "dashboard.png"
     },
     {
-      name: 'Brewster',
-      description: 'Find local breweries, no matter where life takes you.',
-      github: 'https://github.com/N8ers/brewster',
-      site: 'https://brewster-c62ae.firebaseapp.com/',
-      image: 'brewster.png',
+      name: "Brewster",
+      description: "Find local breweries, no matter where life takes you.",
+      github: "https://github.com/N8ers/brewster",
+      site: "https://brewster-c62ae.firebaseapp.com/",
+      image: "brewster.png"
     },
     {
-      name: 'Vue Electron Weather',
-      description: 'An electron weather app to check the forcast.',
-      github: 'https://github.com/N8ers/vue-electron-weather',
+      name: "Vue Electron Weather",
+      description: "An electron weather app to check the forcast.",
+      github: "https://github.com/N8ers/vue-electron-weather",
       // site: 'https://brewster-c62ae.firebaseapp.com/',
-      image: 'vue-elec-weather.png',
-    },
+      image: "vue-elec-weather.png"
+    }
     // {
     //   name: 'Chat App',
     //   description: 'Chat with your friends using this real time chat app',
@@ -59,20 +59,20 @@ export default {
   ],
   blogs: [
     {
-      name: 'Enough to Git you started',
-      description: 'A beginners guide to using Git.',
-      url: 'https://medium.com/@nathanraysheryak/enough-to-git-you-started-32d941730cb7',
+      name: "Enough to Git you started",
+      description: "A beginners guide to using Git.",
+      url: "https://medium.com/@nathanraysheryak/enough-to-git-you-started-32d941730cb7"
     },
     {
-      name: 'Crafting a Dark Mode switch with Vuetify',
+      name: "Crafting a Dark Mode switch with Vuetify",
       description: 'How to make a "Dark Mode" siwtch with a vuetify app!',
-      url: 'https://dev.to/nathan_sheryak/crafting-a-dark-mode-switch-with-vuetify-2ea8',
+      url: "https://dev.to/nathan_sheryak/crafting-a-dark-mode-switch-with-vuetify-2ea8"
     },
-    // {
-    //   name: 'How to save user data in an electron app',
-    //   description: 'A way to save user preferences on an electron app w/o internet or db',
-    //   url: '',
-    // },
+    {
+      name: "My Notes on Postgres",
+      description: "Living documentation where I keep things I've learned about postgres.",
+      url: "https://n8ers.github.io/my-postgres-docs/"
+    }
     // {
     //   name: 'How to build your own NPM package',
     //   description: 'A Guide to publishing an npm package of your own',
@@ -101,5 +101,5 @@ export default {
     //   description: '',
     //   url: '',
     // },
-  ],
+  ]
 };
